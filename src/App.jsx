18344def095fad763acd33677f887734198f8fe6
Rishi Sanjaykumar Dakoria <rishid2386@gmail.com>
2026-05-13 19:55:23 +0530

    completed homepage by adding drink lists and another two leaves at the bottom of the page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import {gsap} from "gsap/gsap-core";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./Components/Navbar.jsx";
 import Hero from "./Components/Hero.jsx";
+import Cocktails from "./Components/Cocktails.jsx";
 gsap.registerPlugin(ScrollTrigger, SplitText);
+
 const App = () => {
     return (
         <main>
             <Navbar />
             <Hero />
-            <div className="h-dvh bg-black"/>
+            <Cocktails />
         </main>
     )
 }
